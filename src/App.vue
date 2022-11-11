97e5b805bd477cb3bc6e6848d8f94658fa70adc8
Delowar Hossain <delowardev@gmail.com>
2022-11-11 15:25:13 +0600

    init

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <appEmojiPicker :native="true" @select="onSelectEmoji" />
+</template>
+
+<script>
+import EmojiPicker from 'vue3-emoji-picker';
+import 'vue3-emoji-picker/css';
+
+import { ref } from 'vue';
+
+export default {
+  name: 'EmojiPicker',
+  emits: ['select'],
+  components: {
+    appEmojiPicker: EmojiPicker,
+  },
+  setup() {
+    const input = ref('');
+
+    function onSelectEmoji(emoji) {
+      console.log(emoji);
+      input.value += emoji.i;
+    }
+
+    function showEmoji(emoji) {
+      console.log(emoji);
+    }
+
+    return {
+      input,
+      onSelectEmoji,
+    };
+  },
+};
+</script>
